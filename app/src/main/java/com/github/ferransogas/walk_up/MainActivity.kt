92a57284c4cbf351b290331b51465e4c7a4da32b
ferran <0ferransogas4@gmail.com>
2025-02-05 23:03:11 +0100

    transition from alarmScreen to editAlarmScreen by tapping the text

--- app/src/main/java/com/github/ferransogas/walk_up/MainActivity.kt
+++ app/src/main/java/com/github/ferransogas/walk_up/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -12,6 +13,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import com.github.ferransogas.walk_up.ui.theme.WalkUpTheme
@@ -22,27 +24,48 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
+        var isEditingAlarm: Boolean by mutableStateOf(false)
+
         setContent {
             WalkUpTheme {
-                Scaffold { innerPadding ->
-                    Column(
-                        modifier = Modifier
-                            .padding(innerPadding)
-                            .fillMaxSize(),
-                        verticalArrangement = Arrangement.Center,
-                        horizontalAlignment = Alignment.CenterHorizontally
-                    ) {
-                        Text(
-                            text = String.format(Locale.getDefault(), "%02d:%02d", 5, 12),
-                            style = MaterialTheme.typography.displayLarge,
-                        )
-                        Switch(
-                            checked = false,
-                            onCheckedChange = {},
-                        )
-                    }
+                if (isEditingAlarm) {
+                    editAlarmScreen()
+                } else {
+                    alarmScreen(onTapBehaviour = { isEditingAlarm = true })
                 }
             }
+        }
+    }
+
+    @Composable
+    private fun alarmScreen(onTapBehaviour: () -> Unit) {
+        Scaffold { innerPadding ->
+            Column(
+                modifier = Modifier
+                    .padding(innerPadding)
+                    .fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    text = String.format(Locale.getDefault(), "%02d:%02d", 5, 12),
+                    style = MaterialTheme.typography.displayLarge,
+                    modifier = Modifier.clickable { onTapBehaviour() }
+                )
+                Switch(
+                    checked = false,
+                    onCheckedChange = {},
+                )
+            }
+        }
+    }
+
+    @Composable
+    private fun editAlarmScreen() {
+        Scaffold {
+            Text(
+                text = "hola"
+            )
         }
     }
 }
